22b25f6bbce1cd43f66eb5f94d4f4654a1a2af79
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-02 12:33:11 -0600

    Migrate users table

---
+++ app/models/user.rb
@@ -0,0 +1,23 @@
+class User < ApplicationRecord
+
+  def new
+
+  end
+
+  def show
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+end
---
+++ db/migrate/20200302175355_create_users.rb
@@ -0,0 +1,15 @@
+class CreateUsers < ActiveRecord::Migration[5.2]
+  def change
+    create_table :users do |t|
+      t.string :username, null: false, index: { unique: true }
+      t.string :email, null: false, index: { unique: true }
+      t.string :gender
+      t.integer :age
+      t.text :bio
+      t.string :password_digest, null: false
+      t.string :session_token, null: false, index: { unique: true }
+
+      t.timestamps
+    end
+  end
+end
