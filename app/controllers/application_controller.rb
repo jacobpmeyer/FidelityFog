07f54e4ffa4c73aaa62ce57cd64baf79b623a92b
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-02 12:41:30 -0600

    Configure routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+
+  helper_method :logged_in?, :current_user
+
+  def current_user
+    @current_user ||= User.find_by(session_token: session[:session_token])  
+  end
+
+  def ensure_session_token
+    redirect_to root_url unless logged_in?
+  end
+
+  def logged_in?
+    !!current_user
+  end
+
+  def login!(user)
+    @current_user = user
+    session[:session_token] = user.reset_session_token!
+  end
+
+  def logout!
+    current_user.reset_session_token!
+    session[:session_token] = nil
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,23 +1,37 @@
 class User < ApplicationRecord
+  attr_reader :password
 
-  def new
+  validates :username, :session_token, :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6, allow_nil: true }
 
+  before_validation :ensure_session_token
+
+  def password=(password)
+      @password = password
+      self.password_digest = BCrypt::Password.create(password)
   end
 
-  def show
-
+  def is_password?(password)
+      BCrypt::Password.new(self.password_digest).is_password?(password)
   end
 
-  def edit
-
+  def reset_session_token!
+    self.session_token = self.class.generate_session_token  
+    self.save!
+    self.session_token
   end
 
-  def update
-
+  def self.find_by_credentials(username, password)
+    @user = User.find_by(username: username)
+    @user && @user.is_password?(password) ? @user : nil
   end
 
-  def destroy
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64(16)
+  end
 
+  def ensure_session_token
+    self.session_token ||= self.class.generate_session_token
   end
 
 end
