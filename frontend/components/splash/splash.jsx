b215acefff154432ec7964d0df191963db085d27
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-04 09:01:57 -0600

    Tune splash message and splash buttons

--- frontend/components/splash/splash.jsx
+++ frontend/components/splash/splash.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NavBarContainer from '../nav_bar/nav_bar_container'
+import { Link } from 'react-router-dom'
 
 class Splash extends React.Component {
   constructor(props) {
@@ -9,17 +10,30 @@ class Splash extends React.Component {
 
   render() {
     return (
-      <div className="splash-main">
+      <div className="banner">
         <NavBarContainer />
-        <div className="splash-message">
-          <div className="splash-header">
-            Discover more with SoundCloud Go+
+        <div className="splash-main">
+          <div className="splash-message">
+            <div className="splash-sub">
+              <div className="splash-header">
+                Discover more with SoundCloud Go+
+              </div>
+              <div className="splash-p">
+                SoundCloud Go+ lets you listen offline, ad-free, 
+                with over 150 million tracks — and growing.
+              </div>
+            </div>
           </div>
-          <div className="splash-p">
-            SoundCloud Go+ lets you listen offline, ad-free, 
-            with over 150 million tracks — and growing.
+          <div className="splash-buttons">
+            <div className="splash-button-1">
+              <Link to="/">Learn more</Link>
+              </div>
+            <div className="splash-button-2">
+              <Link to="/">Try it free for 30 days</Link>
+              </div>
           </div>
         </div>
+        
       </div>
     )
   }
