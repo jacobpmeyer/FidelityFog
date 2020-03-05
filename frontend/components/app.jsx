7292fe9ddb7917516db504a8bd52206cd4a35b6c
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-03-05 12:16:58 -0600

    Create track api util functions

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,16 +1,12 @@
 import React from 'react'
 import { Route, Switch } from 'react-router-dom'
-import SignupFormContainer from './login_form/signup_form_container'
-import LoginFormContainer from './login_form/login_form_container'
 import { AuthRoute } from '../util/route_util'
-import DemoFormContainer from './demo_form/demo_form_container'
 import SplashContainer from './splash/splash_container'
 import Modal from './modal/modal'
 
 const App = () => {
   return (
     <div className="main-content">
-      {console.log(window.currentUser)}
       <Modal />
       <Switch>
         <Route exact path="/" component={SplashContainer} />
---
+++ frontend/util/track_api_util.js
@@ -0,0 +1,36 @@
+export const fetchTracks = () => (
+  $.ajax({
+    url: '/api/tracks',
+    method: 'GET'
+  })
+)
+
+export const fetchTrack = (trackId) => (
+  $.ajax({
+    url: `/api/tracks/${trackId}`,
+    method: 'GET'
+  })
+)
+
+export const createTrack = (track) => (
+  $.ajax({
+    url:`/api/tracks/${track.id}`,
+    method: 'POST',
+    data: { track }
+  })
+)
+
+export const updateTrack = (track) => (
+  $.ajax({
+    url: `/api/tracks/${track.id}`,
+    method: 'PATCH',
+    data: { track }
+  })
+)
+
+export const deleteTrack = (trackId) => (
+  $.ajax({
+    url: `/api/tracks/${trackId}`,
+    method: `DELETE`
+  })
+)
