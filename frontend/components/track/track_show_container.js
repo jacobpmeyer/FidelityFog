55fdc0551a49aea5356b6c7001617ed2f9297c4e
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-15 17:41:01 -0500

    Randomize the track display on the discover page

--- frontend/components/track/track_show_container.js
+++ frontend/components/track/track_show_container.js
@@ -1,11 +1,11 @@
-import { connect } from 'react-redux'
-import TrackShow from './track_show'
-import { fetchTrack, deleteTrack } from '../../actions/track_actions'
+import { connect } from "react-redux";
+import TrackShow from "./track_show";
+import { fetchTrack, deleteTrack } from "../../actions/track_actions";
 import {
   updateCurrentTrack,
   pauseTrack,
-  playTrack
-} from '../../actions/track_ui_actions'
+  playTrack,
+} from "../../actions/track_ui_actions";
 
 const mSTP = (state, ownProps) => {
   return {
@@ -13,15 +13,15 @@ const mSTP = (state, ownProps) => {
     track: state.entities.tracks[ownProps.match.params.trackId],
     currentTrack: state.ui.currentTrack,
     playing: state.ui.playing,
-  }
-}
+  };
+};
 
-const mDTP = dispatch => ({
+const mDTP = (dispatch) => ({
   fetchTrack: (trackId) => dispatch(fetchTrack(trackId)),
   deleteTrack: (trackId) => dispatch(deleteTrack(trackId)),
-  updateCurrentTrack: track => dispatch(updateCurrentTrack(track)),
+  updateCurrentTrack: (track) => dispatch(updateCurrentTrack(track)),
   playTrack: () => dispatch(playTrack()),
   pauseTrack: () => dispatch(pauseTrack()),
-})
+});
 
-export default connect(mSTP, mDTP)(TrackShow)
+export default connect(mSTP, mDTP)(TrackShow);
--- frontend/components/user_discover/user_discover.jsx
+++ frontend/components/user_discover/user_discover.jsx
@@ -4,26 +4,39 @@ import DiscoverTracksContainer from "./discover_tracks_container";
 class UserDiscover extends React.Component {
   constructor(props) {
     super(props);
+    this.state = {
+      tracks: null,
+    };
+    this.shuffleTracks = this.shuffleTracks.bind(this);
   }
 
   componentDidMount() {
+    window.scrollTo(0, 0);
     const { tracks, fetchTrack, fetchAllTracks } = this.props;
     if (tracks.length === 0) {
       // fetchTrack("1");
       // fetchTrack("2");
-      fetchAllTracks();
+      fetchAllTracks().then(() => {
+        const { tracks } = this.props;
+        this.setState({ tracks: this.shuffleTracks(tracks) });
+      });
+    } else {
+      this.setState({ tracks: this.shuffleTracks(tracks) });
     }
   }
 
-  render() {
-    const { tracks } = this.props;
-
+  shuffleTracks(tracks) {
     for (let i = tracks.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * i);
       const temp = tracks[i];
       tracks[i] = tracks[j];
       tracks[j] = temp;
     }
+    return tracks;
+  }
+
+  render() {
+    const { tracks } = this.state;
 
     if (tracks === null) {
       return null;
