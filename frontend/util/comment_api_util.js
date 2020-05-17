d628c286e81922500d6b01cd02f46b6d64716611
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-17 12:20:32 -0500

    Create api_util functions for frontend comments

---
+++ frontend/util/comment_api_util.js
@@ -0,0 +1,31 @@
+export const fetchAllComments = () =>
+  $.ajax({
+    url: "/api/comments",
+    method: "GET",
+  });
+
+export const fetchComment = (commentId) =>
+  $.ajax({
+    url: `/api/comments/${commentId}`,
+    method: "GET",
+  });
+
+export const createComment = (comment) =>
+  $.ajax({
+    url: `/api/comments`,
+    method: `POST`,
+    data: { comment },
+  });
+
+export const updateComment = (comment) =>
+  $.ajax({
+    url: `/api/comment/${comment.id}`,
+    method: `PATCH`,
+    data: { comment },
+  });
+
+export const deleteComment = (commentId) =>
+  $.ajax({
+    url: `/api/comments/${commentId}`,
+    method: `DELETE`,
+  });
