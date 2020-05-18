d9e322024db23400717aeddc55838d4d969f6169
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-18 12:39:28 -0500

    Remove unique index from user_id column of comments table

---
+++ db/migrate/20200518173810_add_user_id_from_comments.rb
@@ -0,0 +1,5 @@
+class AddUserIdFromComments < ActiveRecord::Migration[5.2]
+  def change
+    add_column :comments, :user_id, :integer, null: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_18_173112) do
+ActiveRecord::Schema.define(version: 2020_05_18_173810) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,11 +38,10 @@ ActiveRecord::Schema.define(version: 2020_05_18_173112) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "track_id", null: false
-    t.index ["user_id"], name: "index_comments_on_user_id", unique: true
+    t.integer "user_id", null: false
   end
 
   create_table "tracks", force: :cascade do |t|
