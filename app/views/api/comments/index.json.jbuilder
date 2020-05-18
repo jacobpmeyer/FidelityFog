c46006537a34d3a7012596a09445c444d069aa77
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-18 14:52:55 -0500

    Have the comments index conditionally return the user's avatar

--- app/views/api/comments/index.json.jbuilder
+++ app/views/api/comments/index.json.jbuilder
@@ -1,9 +1,10 @@
 @comments.each do |comment|
-  p comment
   json.set! comment.id do 
     json.extract! comment, :id, :body, :user_id, :track_id
 
     json.username comment.user.username
-    json.avatar comment.user.avatar
+    if comment.user.avatar.attached?
+      json.avatar url_for(comment.user.avatar)
+    end
   end
 end
--- frontend/components/comments/CommentItem.js
+++ frontend/components/comments/CommentItem.js
@@ -1,9 +1,13 @@
 import React from "react";
 
 const CommentItem = ({ comment }) => {
+  const avatar = comment.avatar ? comment.avatar : null;
+
+  console.log(avatar);
+
   return (
     <div className="comment-item">
-      <div className="comment-item-avatar">{comment.user.avatar}</div>
+      {/* <div className="comment-item-avatar">{avatar}</div> */}
       <div className="comment-item-content"></div>
       <div className="comment-item-meta"></div>
     </div>
