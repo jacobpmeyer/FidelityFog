1158856afc1f7d41c12df5b36e6aa739c6dfd5fe
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-19 10:31:56 -0500

    Fix splash page trending section

--- frontend/components/trending/Trending.jsx
+++ frontend/components/trending/Trending.jsx
@@ -1,25 +1,32 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import DiscoverTracksContainer from "../user_discover/discover_tracks_container";
 
 export default ({ fetchAllTracks, fetchTrack, tracks }) => {
+  const [trackOrder, setTrackOrder] = useState(null);
+
   useEffect(() => {
     // fetchTrack("1");
     // fetchTrack("2");
-    fetchAllTracks();
-  }, [tracks.length]); // Only re-run the effect if count changes
+    if (!trackOrder) {
+      fetchAllTracks().then(() => {
+        setTrackOrder(shuffleTracks(tracks).slice(0, 12));
+      });
+    }
+  }, [tracks.length]); // Only re-run the effect if tracks.length changes
 
-  for (let i = tracks.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * i);
-    const temp = tracks[i];
-    tracks[i] = tracks[j];
-    tracks[j] = temp;
-  }
+  const shuffleTracks = (tracks) => {
+    for (let i = tracks.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * i);
+      const temp = tracks[i];
+      tracks[i] = tracks[j];
+      tracks[j] = temp;
+    }
+    return tracks;
+  };
 
-  tracks = tracks.slice(0, 12);
-
-  if (tracks === null) {
+  if (trackOrder === null) {
     return null;
   } else {
-    return <DiscoverTracksContainer tracks={tracks} />;
+    return <DiscoverTracksContainer tracks={trackOrder} />;
   }
 };
