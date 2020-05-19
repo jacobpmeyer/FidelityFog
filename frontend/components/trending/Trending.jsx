dbc98da721bad1fa332635caf3eeaae11f26d90d
Jacob Meyer <jacobpmeyer87@gmail.com>
2020-05-18 19:03:39 -0500

    Restore the tracks selection to the splash page

--- frontend/components/trending/Trending.jsx
+++ frontend/components/trending/Trending.jsx
@@ -3,9 +3,9 @@ import DiscoverTracksContainer from "../user_discover/discover_tracks_container"
 
 export default ({ fetchAllTracks, fetchTrack, tracks }) => {
   useEffect(() => {
-    fetchTrack("1");
-    fetchTrack("2");
-    // fetchAllTracks();
+    // fetchTrack("1");
+    // fetchTrack("2");
+    fetchAllTracks();
   }, [tracks.length]); // Only re-run the effect if count changes
 
   for (let i = tracks.length - 1; i > 0; i--) {
